7d7cd4a66a4787c375070c02b3aff6ef5b08a040
Vishal Suresh <haashiksuresh@gmail.com>
2025-09-04 05:16:58 -0500

    removed constant youtube notes text at top

--- src/sidepanel.tsx
+++ src/sidepanel.tsx
@@ -1,40 +1,20 @@
 import { useState } from "react"
 import { useFirebase } from "./firebase/hook"
 import { Dashboard, LoginPage } from "./components"
-import "./global.css"
+import "./global.module.css"
 
 export default function IndexSidePanel() {
   const { user, isLoading, onLogin } = useFirebase()
   const [currentView, setCurrentView] = useState<'dashboard' | 'new-note' | 'edit-note'>('dashboard')
 
-  const renderMainContent = () => {
-    if (isLoading) {
-      return (
-        <div>
-          <p>Checking authentication...</p>
-        </div>
-      )
-    }
+  if (isLoading)
+    return (
+      <div>
+        <p>Checking authentication...</p>
+      </div>
+    )
 
-    if (!user)
-      return <LoginPage onLogin={onLogin} />
-
-    return <Dashboard onViewChange={setCurrentView} />
-  }
-
-  return (
-    <div>
-      {currentView === 'dashboard' && (
-        <header>
-          <h1>
-            YouTube Notes
-          </h1>
-        </header>
-      )}
-
-      <main>
-        {renderMainContent()}
-      </main>
-    </div>
-  )
+  if (!user) 
+    return <LoginPage onLogin={onLogin} />
+  return <Dashboard onViewChange={setCurrentView} />
 }
