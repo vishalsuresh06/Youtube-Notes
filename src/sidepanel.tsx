119d3bdc2d91bbf60f12a883454a45da325213f7
Vishal Suresh <haashiksuresh@gmail.com>
2025-09-03 23:33:03 -0500

    Refactored sidepanel to look better and seperated the css using css modules

--- src/sidepanel.tsx
+++ src/sidepanel.tsx
@@ -1,37 +1,69 @@
 import { useFirebase } from "~firebase/hook"
+import styles from "./styles/sidepanel.module.css"
 
 export default function IndexSidePanel() {
   const { user, isLoading, onLogin, onLogout } = useFirebase()
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16,
-        width: "300px"
-      }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      {!user ? (
-        <button onClick={() => onLogin()}>Log in</button>
-      ) : (
-        <button onClick={() => onLogout()}>Log out</button>
-      )}
-      <div>
-        {isLoading ? "Loading..." : ""}
-        {!!user ? (
-          <div>
-            Welcome to Plasmo, {user.displayName} your email address is{" "}
-            {user.email}
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
+  const renderAuthButton = () => {
+    if (!user) {
+      return (
+        <button 
+          className={styles.button} 
+          onClick={onLogin}
+        >
+          Log in
+        </button>
+      )
+    }
 
-      <footer>Crafted by @PlasmoHQ</footer>
+    return (
+      <button 
+        className={styles.button} 
+        onClick={onLogout}
+      >
+        Log out
+      </button>
+    )
+  }
+
+  const renderUserInfo = () => {
+    if (isLoading) {
+      return <span className={styles.loading}>Loading...</span>
+    }
+
+    if (!user) {
+      return null
+    }
+
+    return (
+      <div className={styles.welcome}>
+        Welcome to Plasmo, {user.displayName}! 
+        <br />
+        Your email address is {user.email}
+      </div>
+    )
+  }
+
+  return (
+    <div className={styles.container}>
+      <header>
+        <h1 className={styles.title}>
+          Welcome to your{" "}
+          <a href="https://www.plasmo.com" target="_blank" rel="noopener noreferrer">
+            Plasmo
+          </a>{" "}
+          Extension!
+        </h1>
+      </header>
+
+      <main className={styles.content}>
+        {renderAuthButton()}
+        {renderUserInfo()}
+      </main>
+
+      <footer className={styles.footer}>
+        Crafted by @PlasmoHQ
+      </footer>
     </div>
   )
 }
