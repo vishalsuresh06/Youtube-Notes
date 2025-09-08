7d4cb4b486a0fc12211591c1f7effa2a1b18f89e
Vishal Suresh <haashiksuresh@gmail.com>
2025-09-08 03:37:57 -0500

    Added popup to show the keyboard shortcuts

--- src/components/note/info-popup/InfoPopup.tsx
+++ src/components/note/info-popup/InfoPopup.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
 import styles from './info.module.css'
 
 interface InfoPopupProps {
@@ -10,17 +10,57 @@ interface InfoPopupProps {
 const InfoPopup = ({ open, onClose }: InfoPopupProps) => {
   return (
     <>
-        <Dialog open={open} onClose={onClose} className={styles.dialogContainer}>
+        <Dialog 
+          open={open} 
+          onClose={onClose} 
+          className={styles.dialogContainer}
+        >
             <div className={styles.dialogPanelContainer}>
                 <DialogPanel className={styles.dialogPanel}>
-                    <DialogTitle className={styles.dialogTitle}>Note Shortcuts</DialogTitle>
-                    <Description className={styles.dialogDescription}>A list of helpful shortcuts</Description>
-                    <hr/>
-                    <div className={styles.listContainer}>
-                        <ul className={styles.shortcutList}>
-                            <li className={styles.listTitle}>Add Timestamp</li>
-                            <li className={styles.listTitle}>Auto Save</li>
-                        </ul>
+                    <DialogTitle className={styles.dialogTitle}>Shortcuts</DialogTitle>
+                    <div className={styles.shortcutList}>
+                        <div className={styles.shortcutItem}>
+                            <span className={styles.shortcutKey}>⌘T</span>
+                            <div className={styles.shortcutContent}>
+                                <span className={styles.shortcutName}>Timestamp</span>
+                                <span className={styles.shortcutDescription}>Insert current video time</span>
+                            </div>
+                        </div>
+                        <div className={styles.shortcutItem}>
+                            <span className={styles.shortcutKey}>⌘S</span>
+                            <div className={styles.shortcutContent}>
+                                <span className={styles.shortcutName}>Save</span>
+                                <span className={styles.shortcutDescription}>Save your notes instantly</span>
+                            </div>
+                        </div>
+                        <div className={styles.shortcutItem}>
+                            <span className={styles.shortcutKey}>⌘B</span>
+                            <div className={styles.shortcutContent}>
+                                <span className={styles.shortcutName}>Bold</span>
+                                <span className={styles.shortcutDescription}>Format selected text</span>
+                            </div>
+                        </div>
+                        <div className={styles.shortcutItem}>
+                            <span className={styles.shortcutKey}>⌘I</span>
+                            <div className={styles.shortcutContent}>
+                                <span className={styles.shortcutName}>Italicise</span>
+                                <span className={styles.shortcutDescription}>Emphasize selected text</span>
+                            </div>
+                        </div>
+                        <div className={styles.shortcutItem}>
+                            <span className={styles.shortcutKey}>⌘Y</span>
+                            <div className={styles.shortcutContent}>
+                                <span className={styles.shortcutName}>Youtube</span>
+                                <span className={styles.shortcutDescription}>Quick access to YouTube</span>
+                            </div>
+                        </div>
+                        <div className={styles.shortcutItem}>
+                            <span className={styles.shortcutKey}>⌘←</span>
+                            <div className={styles.shortcutContent}>
+                                <span className={styles.shortcutName}>Dashboard</span>
+                                <span className={styles.shortcutDescription}>Return to notes list</span>
+                            </div>
+                        </div>
                     </div>
                     <button className={styles.closeButton} onClick={onClose}>Close</button>
                 </DialogPanel>
