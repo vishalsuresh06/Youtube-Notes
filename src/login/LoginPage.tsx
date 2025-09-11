2581b7e04efc209b0f4967eee0f725e351bf3512
Vishal Suresh <haashiksuresh@gmail.com>
2025-09-10 20:49:49 -0500

    Added login frontend

--- src/login/LoginPage.tsx
+++ src/login/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import styles from "./login.module.css"
+import styles from "./login.module.scss"
 
 interface LastUser {
   uid: string
--- src/sidepanel.tsx
+++ src/sidepanel.tsx
@@ -5,7 +5,20 @@ import LoginPage from "./login/LoginPage"
 import "./global.module.css"
 
 const sidepanel = () => {
-  return <div></div>
+  return (
+    <div>
+      <LoginPage
+        onLogin={() => {
+          chrome.runtime.sendMessage({ type: "LOGIN" })
+        }}
+        onLoginWithDifferentAccount={() => {
+          chrome.runtime.sendMessage({ type: "LOGIN_WITH_DIFFERENT_ACCOUNT" })
+        }}
+        lastUser={null}
+        isLoading={false}
+      />
+    </div>
+  )
 }
 
 export default sidepanel
